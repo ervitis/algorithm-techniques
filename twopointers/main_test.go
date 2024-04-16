0e5bfdc507513d1239ec4ec8edb1808267a2ca9a
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-04-17 08:46:52 +0900

    two pointer implementation

--- twopointers/main_test.go
+++ twopointers/main_test.go
@@ -13,9 +13,32 @@ func Test_twoPointers(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want *int
+		want []int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "target 9",
+			args: args{
+				nums:   []int{2, 7},
+				target: 9,
+			},
+			want: []int{0, 1},
+		},
+		{
+			name: "target 2, return empty",
+			args: args{
+				nums:   []int{3, 7},
+				target: 2,
+			},
+			want: []int{},
+		},
+		{
+			name: "target 12",
+			args: args{
+				nums:   []int{6, 2, 3, 9, 10, 4, 4},
+				target: 12,
+			},
+			want: []int{0, 6},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
